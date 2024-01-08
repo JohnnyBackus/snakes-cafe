08558e90616e21e7fc8c66515eada827b05012c6
JohnnyBackus <john.p.backus@gmail.com>
2024-01-08 11:52:44 -0800

    init commit

---
+++ snakes_cafe.py
@@ -0,0 +1,42 @@
+print("""
+    **************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+***********************************""")
+
+user_order = input("give me your order:")
+
+print("> ", user_order)
+
+# if user_order == "quit":
